4e6de39fa7cafddf94dbf3de13814feaba83c191
Fateme Aghababaei <faghababaei81@gmail.com>
2024-05-21 10:42:45 +0330

    select tags fixed

--- TodolistApp/app/src/main/java/com/example/todolist/screens/AddScreen.kt
+++ TodolistApp/app/src/main/java/com/example/todolist/screens/AddScreen.kt
@@ -5,6 +5,7 @@ import android.app.TimePickerDialog
 import android.util.Log
 import android.widget.DatePicker
 import android.widget.TimePicker
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -21,15 +22,18 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material.icons.outlined.Cancel
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -56,6 +60,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDirection
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
@@ -81,7 +86,7 @@ fun AddScreen(
     var dueDate by remember { mutableStateOf("") }
     var dueTime by remember { mutableStateOf("") }
     var priority by remember { mutableIntStateOf(0) }
-    var tags by remember { mutableStateOf(emptyList<Tag>()) }
+    var tags by remember { mutableStateOf(listOf<Tag>()) }
     var selectedPriorityIndex by remember { mutableIntStateOf(0) }
     val context = LocalContext.current
 
@@ -89,7 +94,7 @@ fun AddScreen(
 
     LaunchedEffect(key1 = allTags) {
         taskViewModel.getTags(token)
-        Log.v("fatt", allTags.toString())
+        Log.v("fatt", "all tags: $allTags")
     }
 
     // Snack bar state
@@ -115,7 +120,7 @@ fun AddScreen(
                             onCancelClicked()
                         }) {
                             Icon(
-                                imageVector = Icons.Outlined.Cancel,
+                                imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
                                 contentDescription = "profile",
                                 tint = MaterialTheme.colorScheme.primary
                             )
@@ -169,61 +174,53 @@ fun AddScreen(
                 }
                 //Tag
                 item {
-                    Text("برچسب‌ها", style = MaterialTheme.typography.bodyMedium)
-                    LazyRow(
-                        modifier = Modifier.padding(16.dp),
-                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.Start
                     ) {
-                        items(allTags) { tag ->
-                            val isSelected = tags.contains(tag)
+                        Text(
+                            "برچسب‌ها",
+                            style = MaterialTheme.typography.bodyMedium,
+                        )
+                        LazyRow(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start)
+                        ) {
+                            items(allTags) { tag ->
+                                var isSelected by remember { mutableStateOf(tags.contains(tag)) }
 
-                            FilterChip(
-                                selected = isSelected,
-                                onClick = {
-                                    Log.v("fatt", "hmmmm")
-                                    tags = if (isSelected) {
-                                        tags - tag
-                                    } else {
-                                        tags + tag
+                                FilterChip(
+                                    selected = isSelected,
+                                    onClick = {
+                                        tags = if (isSelected) {
+                                            tags.minus(tag)
+                                        } else {
+                                            tags.plus(tag)
+                                        }
+                                        isSelected = !isSelected
+                                    },
+                                    label = { Text(tag.title) },
+                                    colors = FilterChipDefaults.filterChipColors(
+                                        containerColor = MaterialTheme.colorScheme.tertiary,
+                                        selectedContainerColor = MaterialTheme.colorScheme.secondary,
+                                    ),
+//                                    border = FilterChipDefaults.filterChipBorder(
+//                                        borderColor = if (isSelected) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.tertiary,
+//                                        selectedBorderColor = MaterialTheme.colorScheme.secondary,
+//                                        borderWidth = 1.dp
+//                                    ),
+                                    leadingIcon = {
+                                        if (isSelected) {
+                                            Icon(imageVector = Icons.Default.Check, contentDescription = "check")
+                                        }
                                     }
-                                    Log.v("fatt", "update tags: $tags")
-                                },
-                                label = { Text(tag.title) }
-                            )
+                                )
+                            }
                         }
                     }
-
-//                    var searchText by remember { mutableStateOf("") }
-//                    val searchResults = remember { mutableStateListOf<String>() }
-//
-//                    OutlinedTextField(
-//                        value = searchText,
-//                        onValueChange = { searchText = it },
-//                        label = { Text("برچسب", style = MaterialTheme.typography.bodyMedium) },
-//                        textStyle = MaterialTheme.typography.bodyMedium.copy(textDirection = TextDirection.ContentOrLtr),
-//
-//                        modifier = Modifier
-//                            .padding(16.dp)
-//                            .fillMaxWidth(),
-//                        shape = RoundedCornerShape(12.dp),
-//                    )
-//                    Column(modifier = Modifier.padding(16.dp)) {
-//                        var expanded by remember { mutableStateOf(false) }
-//                        SearchResultsList(searchResults = searchResults) { result ->
-//                            if (!items.contains(result)) {
-//                                items += result
-//                            }
-//                            searchText = result
-//                            expanded = false
-//                        }
-//                    }
-
-//                    searchResults.clear()
-//                    for (item in items) {
-//                        if (item.contains(searchText)) {
-//                            searchResults.add(item)
-//                        }
-//                    }
                 }
 
                 // Date with Calendar Icon
@@ -387,7 +384,11 @@ fun AddScreen(
                                 snackBarMessage = "پر کردن همه موارد الزامی است."
                             }
                         }) {
-                        Text(text = "افزودن وظیفه", style = MaterialTheme.typography.titleSmall, color = Color.White)
+                        Text(
+                            text = "افزودن وظیفه",
+                            style = MaterialTheme.typography.titleSmall,
+                            color = Color.White
+                        )
                     }
                 }
             }
