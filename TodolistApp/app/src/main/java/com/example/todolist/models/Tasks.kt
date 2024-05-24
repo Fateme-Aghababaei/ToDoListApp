edccf4f289b7c75993b68b34d433af3cbb1eea86
fateme <84022001+fatemebd@users.noreply.github.com>
2024-05-24 14:54:14 +0330

    add java doc to models

--- TodolistApp/app/src/main/java/com/example/todolist/models/Tasks.kt
+++ TodolistApp/app/src/main/java/com/example/todolist/models/Tasks.kt
@@ -1,3 +1,6 @@
 package com.example.todolist.models
 
+/**
+ * Custom ArrayList subclass for managing a list of tasks.
+ */
 class Tasks : ArrayList<Task>()
--- TodolistApp/app/src/main/java/com/example/todolist/navigation/Screens.kt
+++ TodolistApp/app/src/main/java/com/example/todolist/navigation/Screens.kt
@@ -1,5 +1,9 @@
 package com.example.todolist.navigation
-
+/**
+ * Sealed class representing different screens in the application.
+ *
+ * @property route The route associated with the screen.
+ */
 sealed class Screens (val route: String) {
     data object ScreenLogin: Screens("login")
     data object ScreenSignup: Screens("signup")
@@ -13,3 +17,5 @@ sealed class Screens (val route: String) {
     data object AuthenticationRoute: Screens("auth")
     data object AppRoute: Screens("app")
 }
+
+
